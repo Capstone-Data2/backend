8a8a14813902efdcb29a5894bf6c4702120c60e6
sverrep <sverre.paulsen@code.berlin>
2022-06-16 17:14:06 +0200

    remove import

--- Data2/matches/views.py
+++ Data2/matches/views.py
@@ -11,7 +11,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import requests
-import pandas as pd
 
 db, client = get_db_handle()
 # Create your views here.
