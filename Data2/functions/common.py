b91a1e65782b60aa2cdc00e9cce1b5c1ad27f36f
sverrepaulsen <61409275+sverrep@users.noreply.github.com>
2022-06-27 02:18:55 +0200

    refactoring common response and DB code

---
+++ Data2/functions/common.py
@@ -0,0 +1,26 @@
+from functions.player import findRank
+from utils import get_db_handle
+db, client = get_db_handle()
+match_players =  'matches_players'
+match_data =  'matches_data'
+
+def dataAccess(match_id, hero_id=None):
+    data = db.allmatches.find_one({"match_id": match_id, }, {"_id": 0})
+    rank = findRank(data['avg_rank_tier'])
+    match = db[rank + match_data].find_one({"match_id": match_id, }, {"_id": 0})
+    if hero_id == None:
+        players = list(db[rank + match_players].find({"match_id": match_id}, {"_id": 0}))
+        return data, rank, match, players
+    else:
+        selected_player = db[rank + match_players].find_one({"match_id": match_id, "hero_id": hero_id,}, {"_id": 0})
+        return data, rank, match, selected_player
+
+
+def JSONResponseReturn(params, players):
+    resp = {}
+    for param in params:
+        values = {}
+        for player in players:
+            values.update({player["hero_id"]: player[param]})
+        resp.update({param: values})
+    return resp
--- Data2/utils.py
+++ Data2/utils.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import certifi
 
 load_dotenv()
 
 def get_db_handle():
- client = MongoClient(os.getenv('DATABASE_URL'))
+ client = MongoClient(os.getenv('DATABASE_URL'), tlsCAFile=certifi.where())
  db = client.dota2
  return db, client
