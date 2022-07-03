b0bb9bbd1c05dd32a39932fcf33f548d1688787e
sverrep <sverre.paulsen@code.berlin>
2022-07-03 16:00:28 +0200

    refactoring and testing

--- Data2/model/positionFeatures.py
+++ Data2/model/positionFeatures.py
@@ -1,145 +1,33 @@
 
 def posfeatures(position):
+    common = ['kills', 'deaths', 'assists', 'gpm', 'xpm', 'duration',]
+    common_core = ['last_hits', 'hero_damage',]
+    common_sup = ['stuns', 'obs_placed', 'sen_placed']
+    
     #Carry position 
     if position == 1:
-        return [
-            'kills', 
-            'deaths', 
-            'assists', 
-            'gpm', 
-            'xpm', 
-            'duration',
-            'last_hits',
-            'hero_damage',
-            ]
-    
+        return common + common_core
+    #Midlane position 
     elif position == 2:
-        #Midlane position 
-        return [
-            'kills', 
-            'deaths', 
-            'assists', 
-            'gpm', 
-            'xpm', 
-            'duration',
-            'last_hits',
-            'hero_damage',
-            'rune_pickups'
-            ]
+        return common + common_core + ["rune_pickups"]
+    #Offlane position
     elif position == 3:
-        #Offlane position 
-        return [
-            'kills', 
-            'deaths', 
-            'assists', 
-            'gpm', 
-            'xpm', 
-            'duration',
-            'last_hits',
-            'hero_damage',
-            'stuns',
-            'tower_damage'
-            ]
+        return common + common_core + ["stuns", "tower_damage"]
+    #Roamer position 
     elif position == 4:
-        #Roamer position 
-        return [
-            'kills', 
-            'deaths', 
-            'assists', 
-            'gpm', 
-            'xpm', 
-            'duration',
-            'stuns',
-            'obs_placed',
-            'sen_placed'
-
-            ]
+        return common + common_sup
+    #Wimpy Support position
     elif position == 5:
-        #Wimpy Support position 
-        return [
-            'kills', 
-            'deaths', 
-            'assists', 
-            'gpm', 
-            'xpm', 
-            'duration',
-            'stuns',
-            'obs_placed',
-            'sen_placed'
-            ]
+        return common + common_sup
 
 def positionFiller(playermatch, position):
-    if position == 1:
-        """feature_names = posfeatures(1)
-        features = []
-        for feature in feature_names:
-            features.append(playermatch[feature])
-        return features"""
-        return [
-            playermatch['kills'], 
-            playermatch['deaths'], 
-            playermatch['assists'], 
-            playermatch['gpm'], 
-            playermatch['xpm'], 
-            playermatch['duration'],
-            playermatch['last_hits'],
-            playermatch['hero_damage'],
-        ]
-    if position == 2:
-        return [
-            playermatch['kills'], 
-            playermatch['deaths'], 
-            playermatch['assists'], 
-            playermatch['gpm'], 
-            playermatch['xpm'], 
-            playermatch['duration'],
-            playermatch['last_hits'],
-            playermatch['hero_damage'],
-            playermatch['rune_pickups']
-            ]
-    if position == 3:
-        return [
-            playermatch['kills'], 
-            playermatch['deaths'], 
-            playermatch['assists'], 
-            playermatch['gpm'], 
-            playermatch['xpm'], 
-            playermatch['duration'],
-            playermatch['last_hits'],
-            playermatch['hero_damage'],
-            playermatch['stuns'],
-            playermatch['tower_damage']
-            ]
-    if position == 4:
-        return [
-            playermatch['kills'], 
-            playermatch['deaths'], 
-            playermatch['assists'], 
-            playermatch['gpm'], 
-            playermatch['xpm'], 
-            playermatch['duration'],
-            playermatch['stuns'],
-            playermatch['obs_placed'],
-            playermatch['sen_placed']
-
-            ]
-
-    if position == 5:
-        return [
-            playermatch['kills'], 
-            playermatch['deaths'], 
-            playermatch['assists'], 
-            playermatch['gpm'], 
-            playermatch['xpm'], 
-            playermatch['duration'],
-            playermatch['stuns'],
-            playermatch['obs_placed'],
-            playermatch['sen_placed']
-            ]
+    feature_names = posfeatures(position)
+    features = []
+    for feature in feature_names:
+        features.append(playermatch[feature])
+    return features
 
 
 
-
-
-obj = {"kills": 5, "deaths": 2, "assists": 10, "gpm": 500, "xpm": 400, "duration": 700, "last_hits": 150, "hero_damage": 15000}
-print(positionFiller(obj, 1))
+"""obj = {"kills": 5, "deaths": 2, "assists": 10, "gpm": 500, "xpm": 400, "duration": 700, "last_hits": 150, "hero_damage": 15000}
+print(positionFiller(obj, 1))"""
--- Data2/model/tests/test_positionFeatures.py
+++ Data2/model/tests/test_positionFeatures.py
@@ -1,4 +1,5 @@
-from model.positionFeatures import positionFiller
+from turtle import position
+from model.positionFeatures import positionFiller, posfeatures
 
 obj = {
     "kills": 5, 
@@ -16,9 +17,25 @@ obj = {
     "sen_placed": 3
 }
 
+common_features = ['kills', 'deaths', 'assists', 'gpm', 'xpm', 'duration',]
+common_core_features = ['last_hits', 'hero_damage',]
+common_sup_features = ['stuns', 'obs_placed', 'sen_placed']
+
+common_obj = [obj['kills'], obj['deaths'], obj['assists'], obj['gpm'], obj['xpm'], obj['duration']]
+common_core_obj = [obj['last_hits'], obj['hero_damage']]
+common_supp_obj = [obj['stuns'], obj["obs_placed"], obj["sen_placed"]]
+
+def test_posfeatures():
+    assert posfeatures(1) == common_features + common_core_features
+    assert posfeatures(2) == common_features + common_core_features + ["rune_pickups"]
+    assert posfeatures(3) == common_features + common_core_features + ["stuns", "tower_damage"]
+    assert posfeatures(4) == common_features + common_sup_features
+    assert posfeatures(5) == common_features + common_sup_features
+
 def test_positionFiller():
-    assert positionFiller(obj, 1) == [obj['kills'], obj['deaths'], obj['assists'], obj['gpm'], obj['xpm'], obj['duration'], obj['last_hits'], obj['hero_damage']]
-    assert positionFiller(obj, 2) == [obj['kills'], obj['deaths'], obj['assists'], obj['gpm'], obj['xpm'], obj['duration'], obj['last_hits'], obj['hero_damage'], obj['rune_pickups']]
-    assert positionFiller(obj, 3) == [obj['kills'], obj['deaths'], obj['assists'], obj['gpm'], obj['xpm'], obj['duration'], obj['last_hits'], obj['hero_damage'], obj['stuns'], obj['tower_damage']]
-    assert positionFiller(obj, 4) == [obj['kills'], obj['deaths'], obj['assists'], obj['gpm'], obj['xpm'], obj['duration'], obj['stuns'], obj["obs_placed"], obj["sen_placed"]]
-    assert positionFiller(obj, 5) == [obj['kills'], obj['deaths'], obj['assists'], obj['gpm'], obj['xpm'], obj['duration'], obj['stuns'], obj["obs_placed"], obj["sen_placed"]]
+    assert positionFiller(obj, 1) == common_obj + common_core_obj
+    assert positionFiller(obj, 2) == common_obj + common_core_obj + [obj['rune_pickups']]
+    assert positionFiller(obj, 3) == common_obj + common_core_obj + [obj['stuns'], obj['tower_damage']]
+    assert positionFiller(obj, 4) == common_obj + common_supp_obj
+    assert positionFiller(obj, 5) == common_obj + common_supp_obj
+
