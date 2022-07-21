6b71c8d0ae7c6c6642ee3aff1c2b1463b8f0f857
steveno06 <stevenohrdorf@gmail.com>
2022-07-21 13:34:21 +0200

    hotfix

--- Data2/functions/common.py
+++ Data2/functions/common.py
@@ -4,13 +4,16 @@ db, client = get_db_handle()
 match_players =  'matches_players'
 match_data =  'matches_data'
 
-def dataAccess(match_id, hero_id=None):
+def dataAccess(match_id, filter=0, hero_id=None ):
     data = db.allmatches.find_one({"match_id": match_id, }, {"_id": 0})
-    rank = findRank(data['avg_rank_tier'])
+    if data is not None:
+        rank = findRank(data['avg_rank_tier'])
+    elif filter == "1" or data is None:
+        data = db.promatches.find_one({"match_id": match_id, }, {"_id": 0})
+        rank = "pro"
     match = db[rank + match_data].find_one({"match_id": match_id, }, {"_id": 0})
     if hero_id == None:
         players = []
-        
         for player in match["players"]:
             players.append(db[rank + match_players].find_one({"_id": player["_id"]}, {"_id": 0}))
 
