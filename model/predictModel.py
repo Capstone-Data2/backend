5ccfbd3b90a9de6438c00dd01e441717e83f36e0
sverrepaulsen <61409275+sverrep@users.noreply.github.com>
2022-07-25 01:08:10 +0200

    rounding ml score

--- model/predictModel.py
+++ model/predictModel.py
@@ -7,7 +7,7 @@ cwd = os.getcwd()  # Get the current working directory (cwd)
 def predictModel(rank, pos, player):
     win_model = joblib.load(f'{cwd}/model/models/{rank}/pos{pos}_win.sav')
     y = win_model.predict_proba(player)
-    return(y[0][1])
+    return(round(y[0][1], 6))
 
 
 #print(predictModel('herald', 1, player))
--- model/tests/test_model_prediction.py
+++ model/tests/test_model_prediction.py
@@ -11,11 +11,11 @@ player_5= [[4, 2, 6, 500, 500, 1800, 34, 5, 8]]
 
 class TestModelPredicttion:
     def test_prediction(self):
-        assert predictModel('herald', 1, bad_player_1) == 0.06206617257497607
-        assert predictModel('herald', 1, good_player_1) == 0.9878395720283559
+        assert predictModel('herald', 1, bad_player_1) == 0.062066
+        assert predictModel('herald', 1, good_player_1) == 0.98784
 
-        assert predictModel('crusader', 2, player_2) == 0.9665500651350488
-        assert predictModel('archon', 3, player_3) == 0.9541003627111506
+        assert predictModel('crusader', 2, player_2) == 0.96655
+        assert predictModel('archon', 3, player_3) == 0.9541
 
-        assert predictModel('crusader', 4, player_4) == 0.9554857455542284
-        assert predictModel('archon', 5, player_5) == 0.9124103273138414
+        assert predictModel('crusader', 4, player_4) == 0.955486
+        assert predictModel('archon', 5, player_5) == 0.912410
